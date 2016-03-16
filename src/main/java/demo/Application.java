f56447e9828bd67f4f8929512f092abd22d957ba
RichardCSantana <richardcsantana@gmail.com>
2016-03-16 17:27:24 -0300

    ajuste para uso do config-server

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -2,12 +2,14 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringCloudApplication
+@EnableResourceServer
 public class Application {
 
-	public static void main(final String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
