a6226d7240bbac6bdd1547ec50671a41f816e131
Marcos Barbero <marcos.hgb@gmail.com>
2016-06-02 18:14:59 -0300

    Add authorization code grant

--- src/main/java/demo/config/AuthorizationServerConfig.java
+++ src/main/java/demo/config/AuthorizationServerConfig.java
@@ -5,7 +5,6 @@ import java.security.KeyPair;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
--- src/main/java/demo/web/resources/UserDetailsController.java
+++ src/main/java/demo/web/resources/UserDetailsController.java
@@ -2,17 +2,26 @@ package demo.web.resources;
 
 import java.security.Principal;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author marcos.barbero
  */
+@Slf4j
 @RestController
 public class UserDetailsController {
 
 	@RequestMapping("/me")
 	public Principal authenticatedUser(Principal principal) {
 		return principal;
+	}
+
+	@RequestMapping("/authcode")
+	public void code(@RequestParam String code) {
+		log.info("The received code was {}", code);
 	}
 }
